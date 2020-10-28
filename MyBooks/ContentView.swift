4f469f78fddb1a145843b78ba246e319515a8e46
CodeWhore0213 <rahul.anand@bell.ca>
2020-10-28 22:20:28 +0530

    initial commit

---
+++ MyBooks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyBooks
+//
+//  Created by rahul anand on 28/10/20.
+//  Copyright © 2020 rahul anand. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
